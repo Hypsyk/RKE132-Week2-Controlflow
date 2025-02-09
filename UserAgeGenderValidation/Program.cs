3abf7ed2184ef98913c281ffda40ee69e17891f6
Rasmus T <rasmust@Rasmuss-MBP-2.lan>
2025-02-09 20:27:30 +0200

    add week2 code

---
+++ UserAgeGenderValidation/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib vanust
+//kui vanus <13 ss konsoolis kuvatakse:
+//"You are too young to use Instagram"
+//muul juhul kuvatakse "Welcome to Instagram"
+// "13"- heap, 13 - stack
+
+Console.WriteLine("Enter your age:");
+
+int userAge = int.Parse(Console.ReadLine()); //mac ei lase int32 panna
+
+if(userAge >= 13)
+{
+    Console.WriteLine("Welcome to Instagram!");
+}
+else
+{
+    Console.WriteLine("You are too young to use Instagram.");
+}
